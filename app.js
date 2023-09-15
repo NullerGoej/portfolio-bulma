8c544e62f76d8a10e6913ed88ee4d042900d4b04
NullerGoej <bastian2010@live.dk>
2023-09-15 10:07:10 +0200

    Cleaned Code

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    // Initial class for the navbar
     var $navbar = $('.navbar');
     var navbarDefaultClass = 'transparent-navbar';
     var navbarFilledClass = 'filled-navbar';
-    var scrollOffset = 100; // Adjust this value based on your design
+    var scrollOffset = 100;
 
-    // Check scroll position and update the navbar class
     $(window).scroll(function() {
         if ($(this).scrollTop() > scrollOffset) {
             $navbar.removeClass(navbarDefaultClass).addClass(navbarFilledClass);
@@ -17,18 +15,12 @@ $(document).ready(function() {
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-    // Add a click event on each of them
     $navbarBurgers.forEach(el => {
         el.addEventListener('click', () => {
-
-            // Get the target from the "data-target" attribute
             const target = el.dataset.target;
             const $target = document.getElementById(target);
-
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
             el.classList.toggle('is-active');
             $target.classList.toggle('is-active');
 
